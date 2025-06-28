df05bc931af61f9554b7ed0ab726a0ffb007e282
Sasiganesh <sasikumar941@gmail.com>
2025-06-28 20:48:50 +0530

    Initial commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AspnetCoreMvcFull.Models;
+
+
+
+
+namespace AspnetCoreMvcFull.Controllers;
+public class AuthController : Controller
+{
+  public IActionResult ForgotPasswordBasic() => View();
+  public IActionResult LoginBasic() => View();
+  public IActionResult RegisterBasic() => View();
+}
